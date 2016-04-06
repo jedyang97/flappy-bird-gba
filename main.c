ea1ea02766f7fcff5749c34d25adaef37a87936a
Jianing Yang <jed970610@gmail.com>
2016-04-06 05:30:31 -0400

    improve HW09

--- main.c
+++ main.c
@@ -211,7 +211,7 @@ void drawPipes(PIPE pipes[]) {
 }
 
 void drawPipe(PIPE *pipe) {
-    if (pipe->col > SCREEN_WIDTH || pipe->col < 0) {
+    if (pipe->col > SCREEN_WIDTH || pipe->col < 0 - pipeNeckWidth) {
         return;
     }
     for (int i = 0; i < pipe->topHeight - pipeNeckHeight; ++i) {
