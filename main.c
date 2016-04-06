4ebd30788795ba5a589554ecb6c662185aada91d
Jianing Yang <jed970610@gmail.com>
2016-04-06 09:46:03 -0400

    improve HW09

--- main.c
+++ main.c
@@ -250,7 +250,7 @@ void flyLess(BIRD *bird1) {
 void movePipes(PIPE pipes[]) {
     for (int i = 0; i < numPipes; ++i) {
         pipes[i].col -= pipeSpeed;
-        if (pipes[i].col > 0 && pipes[i].col < SCREEN_WIDTH - pipeNeckWidth) {
+        if (pipes[i].col < SCREEN_WIDTH - pipeNeckWidth && pipes[i].col > 0) {
             pipes[i].showing = 1;
         } else {
             pipes[i].showing = 0;
