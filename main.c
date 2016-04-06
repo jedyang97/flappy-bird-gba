9ab654be32fc556143c53768bddd16f39ce6e7e8
Jianing Yang <jed970610@gmail.com>
2016-04-06 04:12:28 -0400

    improve HW09

--- main.c
+++ main.c
@@ -135,7 +135,7 @@ void reset(BIRD *bird1, PIPE pipes[]) {
 
     enablePipe(pipes);
     pipes[0].current = 1;
-    for (int i = 0; i < numPipes; ++i) {
+    for (int i = 1; i < numPipes; ++i) {
         pipes[i].showing = 0;
         pipes[i].current = 0;
         generatePipeHeight(pipes + i);
