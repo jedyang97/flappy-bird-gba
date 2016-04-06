eb116e72f4088bd04d22226aa3fd68f7cfeedbf7
Jianing Yang <jed970610@gmail.com>
2016-04-05 22:13:31 -0400

    improve HW09

--- mylib.c
+++ mylib.c
@@ -36,7 +36,7 @@ void fillScreen(volatile u16 color) {
 void drawImage3(int r, int c, int width, int height, const u16 *image) {
     for (int row = 0; row < height; ++row) {
         DMA[DMA_CHANNEL_3].src = image;
-        DMA[DMA_CHANNEL_3].dst = videoBuffer + OFFSET(r, c, width);
+        DMA[DMA_CHANNEL_3].dst = videoBuffer + OFFSET(row, c, width);
         DMA[DMA_CHANNEL_3].cnt = width |
                                  DMA_SOURCE_INCREMENT |
                                  DMA_DESTINATION_INCREMENT |
