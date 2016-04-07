9fe97fd3a730fe70efc7ad8d162b915d2b2f4e41
Jianing Yang <jed970610@gmail.com>
2016-04-06 22:33:20 -0400

    improve HW09

--- main.c
+++ main.c
@@ -241,9 +241,9 @@ void drawPipe(PIPE *pipe) {
             drawImage3FromCol(i, 0, -pipe->col - pipeMargin, pipeBodyWidth+1, pipeBodyHeight,
                               pipeBody);
         }
-        drawImage3FromCol(pipe->topHeight - pipeNeckHeight, -2, -pipe->col, pipeNeckWidth, pipeNeckHeight,
+        drawImage3FromCol(pipe->topHeight - pipeNeckHeight, 0, -pipe->col, pipeNeckWidth, pipeNeckHeight,
                           pipeNeckTop);
-        drawImage3FromCol(pipe->topHeight + pipe->gapHeight, -2, -pipe->col, pipeNeckWidth, pipeNeckHeight,
+        drawImage3FromCol(pipe->topHeight + pipe->gapHeight, 0, -pipe->col, pipeNeckWidth, pipeNeckHeight,
                           pipeNeckBottom);
         for (int i = 0; i < SCREEN_HEIGHT - (pipe->topHeight + pipe->gapHeight + pipeNeckHeight); ++i) {
             drawImage3FromCol(i + pipe->topHeight + pipe->gapHeight + pipeNeckHeight, 0, -pipe->col - pipeMargin,
@@ -276,10 +276,10 @@ void undrawPipeRear(PIPE *pipe, const u16 *image) {
     for (int i = 0; i < pipe->topHeight - pipeNeckHeight; ++i) {
         undrawImage3(i, pipe->col + pipeMargin + pipeBodyWidth, pipeSpeed, pipeBodyHeight, image);
     }
-    undrawImage3(pipe->topHeight - pipeNeckHeight, pipe->col + pipeNeckWidth, pipeSpeed, pipeNeckHeight, image);
-    undrawImage3(pipe->topHeight + pipe->gapHeight, pipe->col + pipeNeckWidth, pipeSpeed, pipeNeckHeight, image);
+    undrawImage3(pipe->topHeight - pipeNeckHeight, pipe->col + pipeNeckWidth, pipeSpeed +1, pipeNeckHeight, image);
+    undrawImage3(pipe->topHeight + pipe->gapHeight, pipe->col + pipeNeckWidth, pipeSpeed+1, pipeNeckHeight, image);
     for (int i = 0; i < SCREEN_HEIGHT - (pipe->topHeight + pipe->gapHeight + pipeNeckHeight); ++i) {
-        undrawImage3(i + pipe->topHeight + pipe->gapHeight + pipeNeckHeight, pipe->col + pipeMargin + pipeBodyWidth,
+        undrawImage3(i + pipe->topHeight + pipe->gapHeight + pipeNeckHeight, pipe->col + pipeMargin +1+ pipeBodyWidth,
                      pipeSpeed, pipeBodyHeight, image);
     }
 }
