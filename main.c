1f5918a74aece7fe341f3a5397c301ebd30fb3ab
Jianing Yang <jed970610@gmail.com>
2016-04-06 22:14:33 -0400

    improve HW09

--- main.c
+++ main.c
@@ -241,9 +241,9 @@ void drawPipe(PIPE *pipe) {
             drawImage3FromCol(i, 0, -pipe->col - pipeMargin-1, pipeBodyWidth, pipeBodyHeight,
                               pipeBody);
         }
-        drawImage3FromCol(pipe->topHeight - pipeNeckHeight, 0, -pipe->col, pipeNeckWidth-1, pipeNeckHeight,
+        drawImage3FromCol(pipe->topHeight - pipeNeckHeight, 0, -pipe->col, pipeNeckWidth+1, pipeNeckHeight,
                           pipeNeckTop);
-        drawImage3FromCol(pipe->topHeight + pipe->gapHeight, 0, -pipe->col, pipeNeckWidth-1, pipeNeckHeight,
+        drawImage3FromCol(pipe->topHeight + pipe->gapHeight, 0, -pipe->col, pipeNeckWidth+1, pipeNeckHeight,
                           pipeNeckBottom);
         for (int i = 0; i < SCREEN_HEIGHT - (pipe->topHeight + pipe->gapHeight + pipeNeckHeight); ++i) {
             drawImage3FromCol(i + pipe->topHeight + pipe->gapHeight + pipeNeckHeight, 0, -pipe->col - pipeMargin-1,
