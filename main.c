0f65355d79761af59b6dd6adce886aec599cd6de
Jianing Yang <jed970610@gmail.com>
2016-04-06 21:09:40 -0400

    improve HW09

--- main.c
+++ main.c
@@ -237,18 +237,18 @@ void drawPipe(PIPE *pipe) {
         return;
     }
     if (pipe->col < 0) {
-        for (int i = 0; i < pipe->topHeight - pipeNeckHeight; ++i) {
-            drawImage3FromCol(i, 0, -pipe->col - pipeMargin, pipeBodyWidth, pipeBodyHeight,
-                              pipeBody);
-        }
-        drawImage3FromCol(pipe->topHeight - pipeNeckHeight, 0, -pipe->col, pipeNeckWidth, pipeNeckHeight,
-                          pipeNeckTop);
-        drawImage3FromCol(pipe->topHeight + pipe->gapHeight, 0, -pipe->col, pipeNeckWidth, pipeNeckHeight,
-                          pipeNeckBottom);
-        for (int i = 0; i < SCREEN_HEIGHT - (pipe->topHeight + pipe->gapHeight + pipeNeckHeight); ++i) {
-            drawImage3FromCol(i + pipe->topHeight + pipe->gapHeight + pipeNeckHeight, 0, -pipe->col - pipeMargin,
-                              pipeBodyWidth, pipeBodyHeight, pipeBody);
-        }
+//        for (int i = 0; i < pipe->topHeight - pipeNeckHeight; ++i) {
+//            drawImage3FromCol(i, 0, -pipe->col - pipeMargin, pipeBodyWidth, pipeBodyHeight,
+//                              pipeBody);
+//        }
+//        drawImage3FromCol(pipe->topHeight - pipeNeckHeight, 0, -pipe->col, pipeNeckWidth, pipeNeckHeight,
+//                          pipeNeckTop);
+//        drawImage3FromCol(pipe->topHeight + pipe->gapHeight, 0, -pipe->col, pipeNeckWidth, pipeNeckHeight,
+//                          pipeNeckBottom);
+//        for (int i = 0; i < SCREEN_HEIGHT - (pipe->topHeight + pipe->gapHeight + pipeNeckHeight); ++i) {
+//            drawImage3FromCol(i + pipe->topHeight + pipe->gapHeight + pipeNeckHeight, 0, -pipe->col - pipeMargin,
+//                              pipeBodyWidth, pipeBodyHeight, pipeBody);
+//        }
     } else {
         for (int i = 0; i < pipe->topHeight - pipeNeckHeight; ++i) {
             drawImage3(i, pipe->col + pipeMargin, pipeBodyWidth, pipeBodyHeight, pipeBody);
