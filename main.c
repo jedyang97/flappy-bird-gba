5b6e4025046670523830c87b3c69e604827dd877
Jianing Yang <jed970610@gmail.com>
2016-04-06 22:24:51 -0400

    improve HW09

--- main.c
+++ main.c
@@ -236,7 +236,7 @@ void drawPipe(PIPE *pipe) {
     if (!pipe->showing) {
         return;
     }
-    if (pipe->col < 0 && pipe->col + pipeNeckWidth >= 0) {
+    if (pipe->col < 0 && pipe->col + pipeNeckWidth >= -1) {
         for (int i = 0; i < pipe->topHeight - pipeNeckHeight; ++i) {
             drawImage3FromCol(i, 0, -pipe->col - pipeMargin, pipeBodyWidth, pipeBodyHeight,
                               pipeBody);
